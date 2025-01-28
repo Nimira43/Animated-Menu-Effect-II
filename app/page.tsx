cef20c902663a03d67f46a686cbb2006c6113200
Nimira43 <n.rabone72@aol.com>
2025-01-28 01:15:27 +0000

    Testing out Tailwind - good

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function page() {
   return (
-    <div>
-      <h1>Solus</h1>
+    <div className='bg-dark p-4'>
+      <h1 className='logo text-primary text-3xl '>Solus</h1>
     </div>
   )
 }
