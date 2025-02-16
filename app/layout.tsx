db54d20a1b4a4b4b83a3ba7d0114a27e9ded6642
Nimira43 <n.rabone72@aol.com>
2025-02-16 01:58:31 +0000

    Created very basic navbar.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
+import { GiStripedSun } from 'react-icons/gi'
+import Link from 'next/link'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -21,7 +23,23 @@ export default function RootLayout({
     <html lang='en'>
       <body className={poppins.className}>
         <nav className='bg-dark text-light p-5 h-24 flex items-center justify-between'>
-
+          <Link href='/'>Solus</Link>
+          <ul>
+            <li>
+              <Link
+                href='/login'
+              >
+                Login
+              </Link>
+            </li>
+            <li>
+              <Link
+                href='/register'
+              >
+                Register
+              </Link>
+            </li>
+          </ul>
         </nav>
         {children}
       </body>
