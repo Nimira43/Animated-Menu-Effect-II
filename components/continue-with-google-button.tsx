653e1b771918546940f3515d3b9387e1e76d6c54
Nimira43 <n.rabone72@aol.com>
2025-03-01 04:54:25 +0000

    Cleaned up google-button component

--- components/continue-with-google-button.tsx
+++ components/continue-with-google-button.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { Button } from './ui/button'
-import { auth } from '@/firebase/client'
 import { useAuth } from '@/context/auth'
 
 export default function ContinueWithGoogleButton() {
