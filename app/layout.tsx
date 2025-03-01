50f48f042a0ea34a33ca68505e0d16bd3ecfe29d
Nimira43 <n.rabone72@aol.com>
2025-03-01 05:19:57 +0000

    Styling the logo and logo text

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
-// import { GiStripedSun } from 'react-icons/gi'
+import { GiCat } from 'react-icons/gi'
 import Link from 'next/link'
 import { AuthProvider } from '@/context/auth'
 import AuthButtons from '@/components/auth-buttons'
@@ -26,7 +26,13 @@ export default function RootLayout({
       <body className={poppins.className}>
         <AuthProvider>
           <nav className='bg-primary text-light p-5 h-24 flex items-center justify-between'>
-            <Link href='/'>Solus</Link>
+            <Link
+              className='logo text-3xl tracking-wider flex gap-2 items-center'
+              href='/'
+            >
+              <GiCat />
+              <span>Solus</span>
+            </Link>
             <ul>
               <li>
                 <AuthButtons />
