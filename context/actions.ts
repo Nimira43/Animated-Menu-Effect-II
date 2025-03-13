cd48938c268fb6c9f9193d71506aa4b970c8401e
Nimira43 <n.rabone72@aol.com>
2025-03-13 07:31:29 +0000

    setToken function  aims to handle user token verification and apply custom claims for an admin user.

--- context/actions.ts
+++ context/actions.ts
@@ -15,7 +15,11 @@ export const setToken = async({
       return
     }
     const userRecord = await auth.getUser(verifiedToken.uid)
-    if (process.env.ADMIN_EMAIL === userRecord.email && userRecord.customClaims?.admin)
+    if (process.env.ADMIN_EMAIL === userRecord.email && !userRecord.customClaims?.admin) {
+      auth.setCustomUserClaims(verifiedToken.uid, {
+        admin: true
+      })
+    }
   } catch (e) {
     console.log(e)
   }  
