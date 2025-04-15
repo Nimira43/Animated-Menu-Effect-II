4359e371b37f5fb5c53db94fe4a81782fe9793d7
Nimira43 <n.rabone72@aol.com>
2025-04-15 03:13:47 +0100

    Decoding the token fetched from firebaseAuthToken using decodeJwt. This is useful if you need to extract information from the token

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import { cookies } from 'next/headers'
 import { NextRequest, NextResponse } from 'next/server'
+import { decodeJwt } from 'jose'
 
 export async function middleware(request: NextRequest) {
   console.log('Middleware', request.url)
@@ -8,6 +9,7 @@ export async function middleware(request: NextRequest) {
   }
   const cookieStore = await cookies()
   const token = cookieStore.get('firebaseAuthToken')
+  const decodedToken = decodeJwt(token)
 } 
 
 export const config = {
