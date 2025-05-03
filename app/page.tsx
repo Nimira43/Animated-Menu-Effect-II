059485d51125b99100cceded3e71b7d9496f057e
Nimira43 <n.rabone72@aol.com>
2025-05-04 00:30:43 +0100

    Tailwind working fine.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { BiSolidCat } from 'react-icons/bi'
+
 export default function Home() {
   return (
-    <div>Solus</div>
+    <div className='bg-dark p-8'>
+      <div className='flex'>
+        <BiSolidCat  className='text-3xl text-light mr-1'/>
+        <h1 className='text-support logo text-4xl'>Solus</h1>
+      </div>
+    </div>
   )
 }
