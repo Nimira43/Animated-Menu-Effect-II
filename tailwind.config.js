36e7502e3f3683ff508b7c27df9570d877d7e2d3
Nimira43 <n.rabone72@aol.com>
2025-05-04 00:19:19 +0100

    Set up theme colours in tailwind.config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'prime': '#008080',
+        'prime-light': '#06f8f8',
+        'prime-dark': '#013737',
+        'support': '#ff4500',
+        'dark': '#111',
+        'light': '#f6ffff;',
+        'grey-dark': '#333',
+        'grey-medium': '#989898',
+        'grey-light': '#ccc',
+        'grey-light-extra': '#eee',
+      },
+    },
   },
   plugins: [],
 }
