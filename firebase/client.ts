f179d94911ee3f2ffe368a10a12c912befbeb261
Nimira43 <n.rabone72@aol.com>
2025-05-31 01:02:02 +0100

    Working on firebase  client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,12 +1,19 @@
-import { initializeApp } from 'firebase/app'
+import { initializeApp, getApps } from 'firebase/app'
+import { Auth, getAuth } from 'firebase/auth'
+import { FirebaseStorage, getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCCAAJByjbCzhPBXpRv9jihN5O6LIXxI7k',
-  authDomain: 'solus-36aba.firebaseapp.com',
-  projectId: 'solus-36aba',
-  storageBucket: 'solus-36aba.firebasestorage.app',
-  messagingSenderId: '1098779179771',
-  appId: '1:1098779179771:web:4e449b73018d99a511ce5e'
+
 }
 
-const app = initializeApp(firebaseConfig);
+const currentApps = getApps() 
+  let auth: Auth
+  let storage: FirebaseStorage
+
+if (!currentApps.length) {
+  const app = initializeApp(firebaseConfig)
+  auth = getAuth(app)
+  storage = getStorage(app)
+} else {
+  app
+}
