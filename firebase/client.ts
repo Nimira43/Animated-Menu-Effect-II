58725d83c9ef01e583f8a6521a8fad30991b63b1
Nimira43 <n.rabone72@aol.com>
2025-05-31 02:28:41 +0100

    Complete most of the Firebase set up.

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,7 +3,12 @@ import { Auth, getAuth } from 'firebase/auth'
 import { FirebaseStorage, getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-
+  apiKey: 'AIzaSyCCAAJByjbCzhPBXpRv9jihN5O6LIXxI7k',
+  authDomain: 'solus-36aba.firebaseapp.com',
+  projectId: 'solus-36aba',
+  storageBucket: 'solus-36aba.firebasestorage.app',
+  messagingSenderId: '1098779179771',
+  appId: '1:1098779179771:web:4e449b73018d99a511ce5e'
 }
 
 const currentApps = getApps() 
